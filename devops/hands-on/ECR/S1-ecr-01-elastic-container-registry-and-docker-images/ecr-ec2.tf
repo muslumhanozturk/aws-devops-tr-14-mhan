c766ef791f41b2e0208c05fc2a057786e431c594
muslumhanozturk <muslumhanozturk@gmail.com>
2023-10-17 18:44:42 +0300

    new

--- devops/hands-on/ECR/S1-ecr-01-elastic-container-registry-and-docker-images/ecr-ec2.tf
+++ devops/hands-on/ECR/S1-ecr-01-elastic-container-registry-and-docker-images/ecr-ec2.tf
@@ -54,7 +54,7 @@ resource "aws_instance" "ecr-instance" {
           systemctl start docker
           systemctl enable docker
           usermod -a -G docker ec2-user
-          curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
+          curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 \
           -o /usr/local/bin/docker-compose
           chmod +x /usr/local/bin/docker-compose
           EOF
